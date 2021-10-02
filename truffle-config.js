a4ecbbd1336091ba1371696df780e0ffd21075b5
Bofan <bofan94@hotmail.com>
2021-10-02 14:59:38 -0400

    completes function enroll and deposit

--- truffle-config.js
+++ truffle-config.js
@@ -7,3 +7,29 @@ module.exports = {
     }
   }
 };
+
+/*
+//Ganache settings
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*", // 5778 is increased gas blockchain
+     // gas: 10000000,
+    },
+  },
+  contracts_directory: './contracts/',
+  contracts_build_directory: './build/contracts/',
+  compilers: {
+    solc: {
+      version: "^0.8.5",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      evmVersion: "petersburg"
+    }
+  }
+}
+*/
